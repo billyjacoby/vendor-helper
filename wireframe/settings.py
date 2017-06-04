3012ec7de17ec417bde3a05785d051cb41516a05
Billy Jacoby <billyjacoby@gmail.com>
2017-06-03 22:57:33 -0400

    debug=False

--- wireframe/settings.py
+++ wireframe/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_PATH = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['django-wireframe.herokuapp.com']
 
