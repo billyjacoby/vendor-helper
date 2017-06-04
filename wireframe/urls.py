29a9411a6ff48f0c0572d6767b4cd664ee1fd8ec
Billy Jacoby <billyjacoby@gmail.com>
2017-06-03 23:07:07 -0400

    commented out static lines

--- wireframe/urls.py
+++ wireframe/urls.py
@@ -23,7 +23,9 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls')),
 ]
 
+"""
 if not settings.DEBUG:
     urlpatterns += patterns('',
         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     )
+"""
