75acfc30be762d93633a08c5be8172a44169daf1
Billy Jacoby <billyjacoby@gmail.com>
2017-06-04 00:20:42 -0400

    updated settings

--- wireframe/settings.py
+++ wireframe/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_PATH = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['django-wireframe.herokuapp.com']
 
@@ -154,9 +154,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_ACCESS_KEY_ID = "AKIAJSUFYMZ3IFD32HYA"
-AWS_SECRET_ACCESS_KEY = "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"
-AWS_STORAGE_BUCKET_NAME = "django-wireframe"
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
