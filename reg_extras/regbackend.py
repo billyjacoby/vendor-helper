845a48bcdf540385d3bc69017d34d962b4f1cc1a
Billy Jacoby <billyjacoby@gmail.com>
2017-06-07 19:58:28 -0400

    fixed regbackend to save new fields to user profile upon registration

--- reg_extras/regbackend.py
+++ reg_extras/regbackend.py
@@ -19,13 +19,17 @@ class MyRegistrationView(RegistrationView):
             new_user_instance = form.save()
         else:
             new_user_instance = (UserProfile.objects.create_user(**form.cleaned_data))
-
+        company = form.cleaned_data['company_name']
         new_user = RegistrationProfile.objects.create_inactive_user(
         new_user = new_user_instance,
         site=site,
         send_email = self.SEND_ACTIVATION_EMAIL,
-        request=self.request
+        request=self.request,
         )
-        signals.user_registered.send(sender=self.__class__, user=new_user, request=self.request)
+        # the following two lines are necessary to add new fields to the user
+        # profile upon registration
+        new_profile = UserProfile.objects.create(user=new_user, company_name=company)
+        new_profile.save()
 
+        signals.user_registered.send(sender=self.__class__, user=new_user, request=self.request)
         return new_user
