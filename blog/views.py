668185386e7edd9fa84eab199af5cd0cb30dbfa4
Billy Jacoby <billyjacoby@gmail.com>
2017-06-13 22:15:25 -0400

    changed return to render to just render

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, render_to_response, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Blog, Category
 
 # Create your views here.
@@ -10,13 +10,13 @@ def blog_home(request):
     content = {'categories': Category.objects.all(),
                 'posts': Blog.objects.all().order_by('-posted')[:5],
                 }
-    return render_to_response('blog/home.html', content)
+    return render(request, 'blog/home.html', content)
 
 def view_post(request, slug):
-    return render_to_response('blog/view_post.html', {'post': get_object_or_404(Blog, slug=slug)})
+    return render(request, 'blog/view_post.html', {'post': get_object_or_404(Blog, slug=slug)})
 
 
 def view_category(request, slug):
     category = get_object_or_404(Category, slug=slug)
-    return render_to_response('blog/view_category.html', {'category': category,
+    return render(request, 'blog/view_category.html', {'category': category,
                                                 'posts': Blog.objects.filter(category=category)[:5]})
