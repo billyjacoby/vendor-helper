9dd6dd71cabd0734140c5cdced318bb6d3736a67
Billy Jacoby <billyjacoby@gmail.com>
2017-06-18 18:22:31 -0400

    fixed issue on dashboard page, updated to autopopulate task date with todays date

--- website/forms.py
+++ website/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from website.models import TaskModel
+from datetime import date
 
 class TaskModelForm(forms.ModelForm):
     name = forms.CharField(required=True)
     location = forms.CharField(required=False)
-    due_date = forms.DateField(widget=forms.SelectDateWidget, required=True)
+    due_date = forms.DateField(widget=forms.SelectDateWidget, required=True, initial=date.today())
     description = forms.CharField(widget=forms.Textarea, required=True)
 
     class Meta:
