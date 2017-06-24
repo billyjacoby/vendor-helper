f01e5289049673e07182dfcb9c346609c6bfcaee
Billy Jacoby <billyjacoby@gmail.com>
2017-06-24 10:37:48 -0400

    fixed middleware issue, added dashboard to authenticated user menu

--- vendor_helper/middleware.py
+++ vendor_helper/middleware.py
@@ -28,9 +28,7 @@ class LoginRequiredMiddleware:
         if path == "accounts/logout/":
             logout(request)
 
-        if request.user.is_authenticated() and url_is_exempt:
-            return redirect(settings.LOGIN_REDIRECT_URL)
-        elif request.user.is_authenticated() or url_is_exempt:
+        if request.user.is_authenticated() or url_is_exempt:
             return None
         else:
             return redirect(settings.LOGIN_URL + "?next=" + redirect_field_name)
