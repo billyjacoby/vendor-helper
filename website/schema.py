a10e65d5588c157cfadacec42f72c13b5ef387f9
Billy Jacoby <billyjacoby@nike.local>
2017-10-07 09:13:15 -0400

    added ability to resolve incentives by supplier

--- website/schema.py
+++ website/schema.py
@@ -11,5 +11,31 @@ class IncentiveModelType(DjangoObjectType):
 class Query(graphene.AbstractType):
     all_incentive_models = graphene.List(IncentiveModelType)
 
+    incentive_model = graphene.Field(IncentiveModelType,
+                                    id=graphene.Int())
+
+    incentive_model_by_supplier = graphene.List(IncentiveModelType,
+                                                supplier=graphene.String())
+
     def resolve_all_incentive_models(self, args, context, info):
         return IncentiveModel.objects.all()
+
+    def resolve_incentive_model(self, args, context, info):
+        id = args.get('id')
+        supplier = args.get('supplier')
+
+        if id is not None:
+            return IncentiveModel.objects.get(pk=id)
+
+        if supplier is not None:
+            return IncentiveModel.objects.filter(supplier=supplier)
+
+        return None
+
+    def resolve_incentive_model_by_supplier(self, args, context, info):
+        supplier = args.get('supplier')
+
+        if supplier is not None:
+            return IncentiveModel.objects.filter(supplier=supplier)
+
+        return None
